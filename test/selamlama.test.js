072cd83c46e9a06c4a43d658ddc92f1dd0575a6c
Eko <alpcelik2013@gmail.com>
2026-09-20 13:50:38 +0300

    feat: expand all greeting response pools

--- test/selamlama.test.js
+++ test/selamlama.test.js
@@ -3,10 +3,9 @@ const assert = require('node:assert/strict');
 const { shouldRequireBotReply, cevapHavuzu } = require('../src/modules/selamlamaUtils');
 
 test('common greeting pools contain expanded response choices', () => {
-    assert.ok(cevapHavuzu.selam.length >= 40);
-    assert.ok(cevapHavuzu.nasılsın.length >= 40);
-    assert.ok(cevapHavuzu['ne yapıyorsun'].length >= 40);
-    assert.ok(cevapHavuzu.iyiyim.length >= 40);
+    for (const [tip, havuz] of Object.entries(cevapHavuzu)) {
+        assert.ok(havuz.length >= 40, `${tip} havuzu genişletilmemiş`);
+    }
 });
 
 test('all health replies require replying to the bot', () => {
